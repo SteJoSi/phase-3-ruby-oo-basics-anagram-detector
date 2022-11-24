e95ef34ca412bc2b97f5eadee655c68de351bc5d
Stephanie Sison <sjsison27@icloud.com>
2022-11-24 10:51:12 -0800

    commit tree 0b4ed9abaed8ef35b1013e8d52ca564ced621681

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        word_letters = @word.split("")
+        array.filter do |word|
+            word.split("").sort == word_letters.sort
+        end
+    end
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w[enlists google inlets banana])
+
+# binding.pry
